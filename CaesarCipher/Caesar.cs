b0d0d914d59c278a191d09e2ddd29c3816180147
VeganKebab <33206742+VeganKebab@users.noreply.github.com>
2020-10-26 23:02:33 +0200

    Initial commit

---
+++ CaesarCipher/Caesar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+
+namespace CaesarCipher
+{
+    public class Caesar
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write shift for caesar cipher:");
+            string shiftS = Console.ReadLine();
+            Console.WriteLine("Write text:");
+            string text = Console.ReadLine();
+            bool isNumber = int.TryParse(shiftS, out int shift);
+            if (!isNumber)
+            {
+                Console.WriteLine("Shift is not a number");
+                return;
+            }
+            string encrypted = Encrypt(shift, text);
+            string decrypted = Decrypt(shift, encrypted);
+
+            Console.WriteLine("Encrypted: " + encrypted);
+            Console.WriteLine("Decrypted: " + decrypted);
+
+        }
+
+        public static string Encrypt(int shift, string text)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLetter(text[i]))
+                {
+                    result += Convert.ToChar(text[i] + shift);
+
+                }
+                else result += Convert.ToChar(text[i]);
+            }
+            return result;
+        }
+
+        public static string Decrypt(int shift, string text)
+        {
+            string result = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLetter(text[i]))
+                {
+                    result += Convert.ToChar(text[i] - shift);
+
+                }
+                else result += Convert.ToChar(text[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ CaesarCipherTests/ProgramTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CaesarCipher;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaesarCipher.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            string text = "defghIJ"; 
+            int shift = 3;
+            string expected = "abcdeFG";
+
+            string result = CaesarCipher.Caesar.Decrypt(shift, text);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void DecryptTest2()
+        {
+            string text = "Muuj suxtotm, ###";
+            int shift = 6;
+            string expected = "Good morning, ###";
+
+            string result = CaesarCipher.Caesar.Decrypt(shift, text);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            string text = "abcdeFG";
+            int shift = 3;
+            string expected = "defghIJ";
+
+            string result = CaesarCipher.Caesar.Encrypt(shift, text);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void EncryptTest2()
+        {
+            string text = "This is Unit test!!";
+            int shift = 2;
+            string expected = "Vjku ku Wpkv vguv!!";
+
+            string result = CaesarCipher.Caesar.Encrypt(shift, text);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
